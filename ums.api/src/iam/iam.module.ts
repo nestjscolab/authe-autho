b77d1decefac3e914d75da4071a87172d33b80fc
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2023-06-18 19:48:36 -0500

    initial commit

---
+++ ums.api/src/iam/iam.module.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { HashingService } from './hashing/hashing.service';
+import { BcryptService } from './hashing/bcrypt.service';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import { User } from 'src/users/entities/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [
+    { provide: HashingService, useClass: BcryptService },
+    BcryptService,
+    AuthenticationService,
+  ],
+  controllers: [AuthenticationController],
+})
+export class IamModule {}
